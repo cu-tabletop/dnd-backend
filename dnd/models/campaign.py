38b7596cbc821cf1f6f61f5fab8452af5f05da5c
Igor <igor.mamaev1@gmail.com>
2025-09-28 13:49:03 +0300

    create campaign model and participation models

---
+++ dnd/models/campaign.py
@@ -0,0 +1,7 @@
+﻿from django.db import models
+
+
+class Campaign(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1023, default="")
+    icon = models.ImageField(upload_to="campaign_icons")
--- dnd/views/upload.py
+++ dnd/views/upload.py
@@ -15,14 +15,23 @@ def upload_character(request: Request):
         return Response(status=status.HTTP_404_NOT_FOUND)
     owner_obj = owner_obj.first()
 
+    campaign_id = request.data.get("campaign_id")
+    if not campaign_id or not isinstance(campaign_id, int):
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    campaign_obj = Campaign.objects.filter(id=campaign_id)
+    if not campaign_obj.exists():
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    campaign_obj = campaign_obj.first()
+
     data = request.data.get("data")
     if not data or not isinstance(data, dict):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    char_obj = Character.objects.create(owner=owner_obj)
+    char_obj = Character.objects.create(owner=owner_obj, campaign=campaign_obj)
     char_obj.save_data(data)
 
     return Response({
         "char_id": char_obj.id,
         "char_owner_id": char_obj.owner_id,
+        "campaign_id": char_obj.campaign_id,
     }, status=status.HTTP_201_CREATED)
