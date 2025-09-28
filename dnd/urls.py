dc672a3c371656dbfc6b91043266fff02da747fb
Igor <igor.mamaev1@gmail.com>
2025-09-28 21:55:07 +0300

    refactor character view

--- dnd/urls.py
+++ dnd/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('ping/', ping, name="ping"),
-    path('character/upload/', upload_character, name="upload character"),
+    path('character/upload/', upload_character_view, name="upload character"),
     path('character/get/', get_character_view, name='get character'),
     path('campaign/create/', create_campaign_view, name='create campaign'),
     path('campaign/get/', get_campaign_info_view, name='get campaign'),
--- dnd/views/__init__.py
+++ dnd/views/__init__.py
@@ -1,4 +1,3 @@
 from .ping import ping
-from .upload import upload_character
-from .get import get_character_view
 from .campaign import create_campaign_view, get_campaign_info_view, add_to_campaign_view, edit_permissions_view
+from .character import upload_character_view, get_character_view
