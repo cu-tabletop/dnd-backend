7ecab579c03981cb1648d463977499a47675771a
Mamaev Igor <77161122+pxc1984@users.noreply.github.com>
2025-09-28 22:01:56 +0300

    cleanup unused class Role (#13)

--- dnd/models/__init__.py
+++ dnd/models/__init__.py
@@ -1,7 +1,6 @@
 from .character import Character
 from .participation import RoomParticipation
 from .participation import CampaignMembership
-from .role import Role
 from .room import Room
 from .player import Player
 from .campaign import Campaign, CampaignSerializer
