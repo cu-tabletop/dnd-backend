7bfa5a6552c0813d2b3b2965c8f6e85e1af5a1a5
Mamaev Igor <77161122+pxc1984@users.noreply.github.com>
2025-09-28 13:08:35 +0300

    refactor /api/character/upload/ endpoint (#7)

--- dnd/urls.py
+++ dnd/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('ping/', ping, name="ping"),
-    path('upload/character/', upload_character, name="upload character"),
+    path('character/upload/', upload_character, name="upload character"),
     path('character/get/', get_character_view, name='get character'),
 ]
