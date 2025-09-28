abb8a1dd67a338e80f1eb24862a033b48f7ca69a
Igor <igor.mamaev1@gmail.com>
2025-09-28 13:07:30 +0300

    refactor /api/character/upload/ endpoint

--- dnd/views/upload.py
+++ dnd/views/upload.py
@@ -1,12 +1,9 @@
-from django.core.files.base import ContentFile
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
 
 from ..models import *
-from ..services import try_load_json
-import json
 
 @api_view(['POST'])
 def upload_character(request: Request):
@@ -15,18 +12,17 @@ def upload_character(request: Request):
         return Response(status=status.HTTP_400_BAD_REQUEST)
     owner_obj = Player.objects.filter(id=owner_id)
     if not owner_obj.exists():
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     owner_obj = owner_obj.first()
 
     data = request.data.get("data")
     if not data or not isinstance(data, dict):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    char_name = request.data.get("char_name")
-    if not char_name or not isinstance(char_name, str):
-        char_name = hash(char_name) % 10000
-
     char_obj = Character.objects.create(owner=owner_obj)
     char_obj.save_data(data)
 
-    return Response(status=status.HTTP_201_CREATED)
+    return Response({
+        "char_id": char_obj.id,
+        "char_owner_id": char_obj.owner_id,
+    }, status=status.HTTP_201_CREATED)
